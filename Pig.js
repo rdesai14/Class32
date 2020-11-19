248e822cae3a9e0fa9b09bccd00321ae36bdb225
Your Name <you@example.com>
2020-11-19 13:59:52 -0500

    changed

--- Pig.js
+++ Pig.js
@@ -16,11 +16,20 @@ class Pig extends BaseClass {
      this.Visiblity = this.Visiblity - 5;
      tint(255,this.Visiblity);
      image(this.image, this.body.position.x, this.body.position.y, 50, 50);
+     
      pop();
    }
    
  }
 
+score(){
 
+if(this.Visibility < 0 && this.Visibility > -1005 ){
+
+score++
+
+}
+
+}
 
 };
